708e825441dfc4fce9dca19dbb77db530de775b9
HoseanRC <hoseandawoodi90@outlook.com>
2023-04-14 17:16:02 +0330

    fixed some errors

--- commands/utils/stats.js
+++ commands/utils/stats.js
@@ -16,6 +16,7 @@ export const data = new SlashCommandBuilder()
  * @param {CommandInteraction} interaction 
  */
 export async function execute(interaction) {
+    let startTime = new Date();
     let name = `${interaction.options.getString("name")}`;
     if (/^[a-zA-Z0-9_]{2,16}$/g.exec(name) == null) {
         return interaction.reply({
@@ -27,14 +28,16 @@ export async function execute(interaction) {
                     })
                     .setTitle("Invalid player name!")
                     .setDescription("please try again.")
+                    .setColor(0xFF0000)
+                    .setTimestamp()
             ],
             ephemeral: true
         });
     }
-    https.get(`https://www.6b6t.org/_next/data/ddf2b40d01eedecbc17b4827a6c34be2d707022c/en/stats/${name}.json`, function (res) {
-        let data = [];
-        res.on("data", data.push);
-        interaction.deferReply();
+    https.get(`https://www.6b6t.org/_next/data/3178aa6f948ad9a592a38e79c7843c7d208881cd/en/stats/${name}.json`, function (res) {
+        let data = "";
+        res.on("data", a => { data = data.concat(a.toString()) });
+        // interaction.deferReply();
         res.on("end", async function () {
             /**
              * @type {{
@@ -58,46 +61,61 @@ export async function execute(interaction) {
              * }|null}
              */
             let userStats = JSON.parse(Buffer.from(data).toString()).pageProps.userStats;
-            interaction.editReply({
-                embeds: [
-                    new EmbedBuilder()
-                        .setAuthor({ name: interaction.user.tag, iconURL: interaction.user.avatarURL() })
-                        .setTitle(`${name}'s stats${userStats === null ? " doesn't exist!" : ""}`)
-                        .setURL(`https://www.6b6t.org/stats/${name}`)
-                        .setDescription(userStats === null ? `*Check if the bot is wrong:*\nhttps://www.6b6t.org/stats/${name}` : null)
-                        .addFields({
-                            name: 'Important Stats',
-                            value: `Playtime - **${secondsToPeriod(userStats.playtime / 20)}**\n` +
-                                `First Played - **At ${new Date(userStats.firstPlayed).toISOString()
-                                    .replace("-", "/").replace("T", " ").replace(/\..*$/g, "")
-                                } ||<t:${userStats.firstPlayed / 1000}:R>||**\n` +
-                                `Blocks Walked - **${addPoints(userStats.walk)}**\n` +
-                                `Blocks Flied - **${addPoints(userStats.fly)}**\n` +
-                                `Player Kills - **${addPoints(userStats.playerKills)}**\n` +
-                                `Deaths - **${addPoints(userStats.deaths)}**\n` +
-                                `K/D Ratio - **${addPoints((userStats.playerKills / userStats.deaths).toFixed(2))}**\n` +
-                                `\n` +
-                                `Totems Popped/Used - **${addPoints(userStats.useTotem)} times**\n` +
-                                `TNT Placed - **${addPoints(userStats.placeTNT)} times**\n` +
-                                `End Crystals Placed - **${addPoints(userStats.placeEndCrystal)} times**\n` +
-                                `Time Since Death - **${secondsToPeriod(addPoints(userStats.timeSinceDeath / 20))}**`
-                        },
-                        {
-                            name: 'Other Stats',
-                            value: `Joined - **${addPoints(userStats.join)} times**\n` +
-                                `Jumped - **${addPoints(userStats.jumps)} times**\n` +
-                                `Mob Kills - **${addPoints(userStats.mobKills)}**\n` +
-                                `Netherrack Mined - **${addPoints(userStats.mineNetherRack)} times**\n` +
-                                `Cakes Eaten - **${addPoints(userStats.cakeSlicesEaten)}**\n` +
-                                `Golden Hoes Crafted - **${addPoints(userStats.goldenHoeCrafts)}**\n` +
-                                `Golden Apples Eaten - **${addPoints(userStats.goldenAppleEaten)}**`
-                        })
-                        .setColor((await getAverageColor(`https://www.6b6t.org/stats/${name}`)).value.slice(0,3)/*Math.floor(Math.random() * 0x1000000)*/)
-                        .setThumbnail(`https://minotar.net/avatar/${name}/300.png`)
-                        .setFooter({text: `Done in ${Math.floor((new Date().getMilliseconds() - interaction.createdAt.getMilliseconds()) / 10) / 100}s`})
-                        .setTimestamp(),
-                ]
-            });
+            if (!userStats)
+                interaction.reply({
+                    embeds: [
+                        new EmbedBuilder()
+                            .setAuthor({
+                                name: interaction.user.tag,
+                                iconURL: interaction.user.avatarURL()
+                            })
+                            .setTitle(`No stats for ${name} found!`)
+                            .setDescription("They may need to rejoin 6b6t to be tracked")
+                            .setColor(0xFF0000)
+                            .setTimestamp()
+                    ]
+                })
+            else
+                interaction.reply({
+                    embeds: [
+                        new EmbedBuilder()
+                            .setAuthor({ name: interaction.user.tag, iconURL: interaction.user.avatarURL() })
+                            .setTitle(`${name}'s stats${userStats === null ? " doesn't exist!" : ""}`)
+                            .setURL(`https://www.6b6t.org/stats/${name}`)
+                            .setDescription(userStats === null ? `*Check if the bot is wrong:*\nhttps://www.6b6t.org/stats/${name}` : null)
+                            .addFields({
+                                name: 'Important Stats',
+                                value: `Playtime - **${secondsToPeriod(userStats.playtime / 20)}**\n` +
+                                    `First Played - **At ${new Date(userStats.firstPlayed).toISOString()
+                                        .replaceAll("-", "/").replace("T", " ").replace(/\..*$/g, "")
+                                    } ||${Math.floor((new Date() - new Date(userStats.firstPlayed)) / 86400000)} Days ago||**\n` +
+                                    `Blocks Walked - **${addPoints(userStats.walk)}**\n` +
+                                    `Blocks Flied - **${addPoints(userStats.fly)}**\n` +
+                                    `Player Kills - **${addPoints(userStats.playerKills)}**\n` +
+                                    `Deaths - **${addPoints(userStats.deaths)}**\n` +
+                                    `K/D Ratio - **${addPoints((userStats.playerKills / userStats.deaths).toFixed(2))}**\n` +
+                                    `\n` +
+                                    `Totems Popped/Used - **${addPoints(userStats.useTotem)} times**\n` +
+                                    `TNT Placed - **${addPoints(userStats.placeTNT)} times**\n` +
+                                    `End Crystals Placed - **${addPoints(userStats.placeEndCrystal)} times**\n` +
+                                    `Time Since Death - **${secondsToPeriod(userStats.timeSinceDeath / 20)}**`
+                            },
+                                {
+                                    name: 'Other Stats',
+                                    value: `Joined - **${addPoints(userStats.join)} times**\n` +
+                                        `Jumped - **${addPoints(userStats.jumps)} times**\n` +
+                                        `Mob Kills - **${addPoints(userStats.mobKills)}**\n` +
+                                        `Netherrack Mined - **${addPoints(userStats.mineNetherRack)} times**\n` +
+                                        `Cakes Eaten - **${addPoints(userStats.cakeSlicesEaten)}**\n` +
+                                        `Golden Hoes Crafted - **${addPoints(userStats.goldenHoeCrafts)}**\n` +
+                                        `Golden Apples Eaten - **${addPoints(userStats.goldenAppleEaten)}**`
+                                })
+                            .setColor((await getAverageColor(`https://minotar.net/avatar/${name}/300.png`)).value.slice(0, 3)/*Math.floor(Math.random() * 0x1000000)*/)
+                            .setThumbnail(`https://minotar.net/avatar/${name}/300.png`)
+                            .setFooter({ text: `Done in ${Math.floor((new Date() - startTime) / 10) / 100}s` })
+                            .setTimestamp(),
+                    ]
+                });
         });
     });
 }
--- utils.js
+++ utils.js
@@ -3,13 +3,14 @@
  * @returns {String}
  */
 export function secondsToPeriod (totalSeconds) {
-    let time = {
-    weeks  : Math.floor(totalSeconds / 604800),
-    days   : Math.floor((totalSeconds - (weeks * 604800)) / 86400),
-    hours  : Math.floor((totalSeconds - (days * 86400) - (weeks * 604800)) / 3600),
-    minutes: Math.floor((totalSeconds - (hours * 3600) - (days * 86400) - (weeks * 604800)) / 60),
-    seconds: Math.ceil(totalSeconds - (hours * 3600) - (minutes * 60) - (days * 86400) - (weeks * 604800)),
-    };
+    let time = { weeks: 0, days: 0, hours: 0, minutes: 0, seconds: 0 };
+
+    time.weeks   = Math.floor(totalSeconds / 604800);
+    time.days    = Math.floor((totalSeconds - (time.weeks * 604800)) / 86400);
+    time.hours   = Math.floor((totalSeconds - (time.days * 86400) - (time.weeks * 604800)) / 3600);
+    time.minutes = Math.floor((totalSeconds - (time.hours * 3600) - (time.days * 86400) - (time.weeks * 604800)) / 60);
+    time.seconds = Math.ceil(totalSeconds - (time.hours * 3600) - (time.minutes * 60) - (time.days * 86400) - (time.weeks * 604800));
+    
     return Object.keys(time).reduce(
         (timeArray, key) => time[key] != 0 ? timeArray.concat(
             [`${time[key]} ${time[key] > 1 ? key : key.substring(0, key.length - 1)}`]
