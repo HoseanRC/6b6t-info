0c21c96bcb4ce9b9fe0536028b11d471b2680ba1
HoseanRC <hoseandawoodi90@outlook.com>
2023-04-15 21:37:42 +0330

    added button handling

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.cooldowns = new Collection();
 client.commands = new Collection();
-const foldersPath = join(__dirname, 'commands');
-const commandFolders = readdirSync(foldersPath);
+const commandFoldersPath = join(__dirname, 'commands');
+const commandFolders = readdirSync(commandFoldersPath);
 
 for (const folder of commandFolders) {
-	const commandsPath = join(foldersPath, folder);
+	const commandsPath = join(commandFoldersPath, folder);
 	const commandFiles = readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 	for (const file of commandFiles) {
 		const filePath = join(commandsPath, file);
@@ -27,45 +27,67 @@ for (const folder of commandFolders) {
 	}
 }
 
+client.buttons = new Collection();
+const buttonsPath = join(__dirname, 'buttons');
+const buttonFiles = readdirSync(buttonsPath);
+for (const file of buttonFiles) {
+	const filePath = join(buttonsPath, file);
+	const button = await import(filePath);
+	if ('data' in button && 'execute' in button) {
+		client.buttons.set(button.data.match, button);
+	} else {
+		console.log(`[WARNING] The button at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
+
 client.once(Events.ClientReady, () => {
 	console.log('Ready!');
 });
 
 client.on(Events.InteractionCreate, async interaction => {
-	if (!interaction.isChatInputCommand()) return;
+	if (interaction.isChatInputCommand()) {
 
-	const command = client.commands.get(interaction.commandName);
+		const command = client.commands.get(interaction.commandName);
 
-	if (!command) return;
+		if (!command) return;
 
-	const { cooldowns } = client;
+		const { cooldowns } = client;
 
-	if (!cooldowns.has(command.name)) {
-		cooldowns.set(command.name, new Collection());
-	}
-
-	const now = Date.now();
-	const timestamps = cooldowns.get(command.name);
-	const defaultCooldownDuration = 3;
-	const cooldownAmount = (command.cooldown ?? defaultCooldownDuration) * 1000;
-
-	if (timestamps.has(interaction.user.id)) {
-		const expirationTime = timestamps.get(interaction.user.id) + cooldownAmount;
-
-		if (now < expirationTime) {
-			const expiredTimestamp = Math.round(expirationTime / 1000);
-			return interaction.reply({ content: `Please wait <t:${expiredTimestamp}:R> more second(s) before reusing the \`${command.name}\` command.`, ephemeral: true });
+		if (!cooldowns.has(command.name)) {
+			cooldowns.set(command.name, new Collection());
 		}
-	}
 
-	timestamps.set(interaction.user.id, now);
-	setTimeout(() => timestamps.delete(interaction.user.id), cooldownAmount);
+		const now = Date.now();
+		const timestamps = cooldowns.get(command.name);
+		const defaultCooldownDuration = 3;
+		const cooldownAmount = (command.cooldown ?? defaultCooldownDuration) * 1000;
 
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		if (timestamps.has(interaction.user.id)) {
+			const expirationTime = timestamps.get(interaction.user.id) + cooldownAmount;
+
+			if (now < expirationTime) {
+				const expiredTimestamp = Math.round(expirationTime / 1000);
+				return interaction.reply({ content: `Please wait <t:${expiredTimestamp}:R> more second(s) before reusing the \`${command.name}\` command.`, ephemeral: true });
+			}
+		}
+
+		timestamps.set(interaction.user.id, now);
+		setTimeout(() => timestamps.delete(interaction.user.id), cooldownAmount);
+
+		try {
+			await command.execute(interaction);
+		} catch (error) {
+			console.error(error);
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	} else if (interaction.isButton()) {
+		/**
+		 * @type {Collection<String | RegExp, {data: {match: String | RegExp}, execute: Promise | Function}>}
+		 */
+		const buttons = client.buttons.filter((value, key) => interaction.customId.match(key) ? true : false);
+		buttons.forEach(async button => {
+			await button.execute(interaction, interaction.customId);
+		});
 	}
 });
 
